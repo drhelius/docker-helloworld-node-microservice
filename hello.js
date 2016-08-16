093ec94ab1f845cae74326a47fe3c4a5318c527a
Ignacio Sanchez Gines <isanchez.gines@atsistemas.com>
2016-08-16 09:49:18 +0200

    response shows serever hostname

--- hello.js
+++ hello.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var os = require("os");
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello World!');
+  res.end('Hello World from host \"' + os.hostname() + '\".');
 });
 
 server.listen(8080);
